38cdadd38d23a0923199a858cd7039a936c347c0
dibss <ludovicad01@gmail.com>
2022-04-28 19:31:16 +0200

    files

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div id="logo">
+
+    </div>
+    <nav id="menu-head">
+
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'HeaderComp',
+}
+</script>
+
+<style scoped lang='scss'>
+</style>
